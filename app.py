558bdaa0317baad7676caf67e105fb5aeebc27f4
DhruveshSolanki <84910531+DhruveshSolanki@users.noreply.github.com>
2022-03-28 21:46:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,6 +39,3 @@ def predict_model():
         out = model.predict(image_array)
     return render_template('result.html', img_data=encoded_img_data.decode('utf-8'), ans=np.argmax(out, axis=1))
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
-    app.run(debug=True)
